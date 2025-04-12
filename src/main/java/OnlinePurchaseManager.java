83ff2c9d3f1b16e1ce77084a97eb0013c38ddb68
EthanLiu2 <etliu1@s.sfusd.edu>
2025-04-11 22:54:12 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -13,7 +13,7 @@ public class OnlinePurchaseManager
   {
     int sum=0;
     for(int i=0;i<purchases.size();i++){
-    if(purchases.get(i).isElectric()&&purchases.get(i).getmaker().equals(maker)){
+    if(purchases.get(i).isElectric()&&purchases.get(i).getMaker().equals(maker)){
     sum++;
     }
     }
